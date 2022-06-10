e89d18493d38e5d4b5f819d5930a9e89f997b836
Gayal Dassanayake <g.c.dassanayake@gmail.com>
2022-06-10 10:52:57 +0530

    Partially complete GraphCommand

--- cli/ballerina-cli/src/main/java/io/ballerina/cli/cmd/GraphCommand.java
+++ cli/ballerina-cli/src/main/java/io/ballerina/cli/cmd/GraphCommand.java
@@ -18,7 +18,16 @@
 
 package io.ballerina.cli.cmd;
 
+import io.ballerina.cli.BLauncherCmd;
+import io.ballerina.cli.utils.ProjectUtils;
+import io.ballerina.projects.BuildOptions;
+import io.ballerina.projects.Project;
+import io.ballerina.projects.ProjectException;
+import io.ballerina.projects.directory.BuildProject;
+import io.ballerina.projects.directory.SingleFileProject;
 import io.ballerina.projects.util.ProjectConstants;
+import org.ballerinalang.toml.exceptions.SettingsTomlException;
+import org.wso2.ballerinalang.util.RepoUtils;
 import picocli.CommandLine;
 
 import java.io.PrintStream;
@@ -26,28 +35,30 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static io.ballerina.cli.cmd.Constants.GRAPH_COMMAND;
+import static org.wso2.ballerinalang.util.RepoUtils.isBallerinaStandaloneFile;
 
 /**
  * This class represents the "bal graph" command.
  *
- * @since
+ * @since 2201.2.0
  */
+// TODO: change the bal graph message in all places.
 @CommandLine.Command(name = GRAPH_COMMAND, description = "bal graph - Print the dependency graph of a Ballerina "
-        + "project in text form")
+        + "project in DOT graph description language")
 public class GraphCommand {
-    private final Path projectPath;
     private final PrintStream outStream;
     private final PrintStream errStream;
     private final boolean exitWhenFinish;
-
-    @CommandLine.Option(names = "--dump-raw-graphs", description = "Print the raw dependency graphs in text form.",
+    @CommandLine.Parameters(arity = "0..1")
+    private final Path projectPath;
+    @CommandLine.Option(names = "--dump-raw-graphs", description = "Print the raw dependency graphs",
             defaultValue = "false")
     private boolean dumpRawGraphs;
-
+    @CommandLine.Option(names = {"--help", "-h"}, hidden = true, defaultValue = "false")
+    private boolean helpFlag;
     @CommandLine.Option(names = {"--offline"}, description = "Print the dependency graph offline without downloading " +
             "dependencies.", defaultValue = "false")
     private boolean offline;
-
     @CommandLine.Option(names = "--sticky", description = "stick to exact versions locked (if exists)",
             defaultValue = "false")
     private boolean sticky;
@@ -68,6 +79,65 @@ public class GraphCommand {
     }
 
     public void execute() {
+        if (this.helpFlag) {
+            String commandUsageInfo = BLauncherCmd.getCommandUsageInfo(GRAPH_COMMAND);
+            this.errStream.println(commandUsageInfo);
+            return;
+        }
 
+        Project project;
+        BuildOptions buildOptions = constructBuildOptions();
+
+        boolean isSingleFileProject = isBallerinaStandaloneFile(this.projectPath);
+        if (isSingleFileProject) {
+            try {
+                project = SingleFileProject.load(this.projectPath, buildOptions);
+            } catch (ProjectException e) {
+                CommandUtil.printError(this.errStream, e.getMessage(), null, false);
+                CommandUtil.exitError(this.exitWhenFinish);
+                return;
+            }
+        } else {
+            try {
+                project = BuildProject.load(this.projectPath, buildOptions);
+            } catch (ProjectException e) {
+                CommandUtil.printError(this.errStream, e.getMessage(), null, false);
+                CommandUtil.exitError(this.exitWhenFinish);
+                return;
+            }
+        }
+
+        // If project is empty
+        if (ProjectUtils.isProjectEmpty(project)) {
+            CommandUtil.printError(this.errStream, "package is empty. Please add at least one .bal file.", null,
+                    false);
+            CommandUtil.exitError(this.exitWhenFinish);
+            return;
+        }
+
+        // Validate Settings.toml file
+        try {
+            RepoUtils.readSettings();
+        } catch (SettingsTomlException e) {
+            this.outStream.println("warning: " + e.getMessage());
+        }
+
+        // TODO: taskbuilder
+
+
+        if (this.exitWhenFinish) {
+            Runtime.getRuntime().exit(0);
+        }
+    }
+
+    private BuildOptions constructBuildOptions() {
+        BuildOptions.BuildOptionsBuilder buildOptionsBuilder = BuildOptions.builder();
+
+        buildOptionsBuilder
+                .setDumpRawGraphs(this.dumpRawGraphs)
+                .setOffline(this.offline)
+                .setSticky(sticky);
+
+        return buildOptionsBuilder.build();
     }
 }
